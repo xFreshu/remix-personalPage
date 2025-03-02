8a19a2c4483ba36b7dcfc24fb81e268a0b44abc8
xFreshu <lprzybyszcontact@gmail.com>
2025-03-02 16:33:05 +0100

    start working on project

---
+++ app/UI/Hero/Hero.tsx
@@ -0,0 +1,10 @@
+import classes from './Hero.module.scss'
+
+export function Hero() {
+  return (
+    <div className={classes.hero}>
+      <h1>Witaj,</h1>
+      <h2>Jestem Łukasz Przybysz, </h2>
+    </div>
+  )
+}
--- app/root.tsx
+++ app/root.tsx
@@ -4,21 +4,23 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node";
+} from '@remix-run/react'
+import type { LinksFunction } from '@remix-run/node'
+import './styles/reset.scss'
+import classes from './styles/root.module.scss'
 
 export const links: LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
+  { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
   {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
+    rel: 'preconnect',
+    href: 'https://fonts.gstatic.com',
+    crossOrigin: 'anonymous',
   },
   {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,100..1000&display=swap',
   },
-];
+]
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -30,14 +32,16 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
+        <div className={classes.wrapper}>
+          {children}
+          <ScrollRestoration />
+          <Scripts />
+        </div>
       </body>
     </html>
-  );
+  )
 }
 
 export default function App() {
-  return <Outlet />;
+  return <Outlet />
 }
